f8c054a48df142a5529d8223ee3075e6a58abaa0
brunaopdejesus <bruna.oliveira.656667@gmail.com>
2021-10-08 11:17:42 -0300

    08-10

--- produtos/acoes.php
+++ produtos/acoes.php
@@ -19,6 +19,26 @@
             // UPLOAD DO ARQUIVO
             move_uploaded_file($_FILES["foto"]["tmp_name"], "fotos/$novoNome");
 
+            $descricao = $_POST["descricao"];
+            $peso = $_POST["peso"];
+            $quantidade = $_POST["quantidade"];
+            $cor = $_POST["cor"];
+            $tamanho = $_POST["tamanho"];
+            $valor = $_POST["valor"];
+            $desconto = $_POST["desconto"];
+            $categoriaId = $_POST["categoria"];
+            
+            $sql = "INSERT INTO tbl_produto (descricao, peso, quantidade, cor, 
+            tamanho, valor, desconto, imagem, categoria_id) 
+            VALUES ('$descricao', $peso, $quantidade, '$cor', '$tamanho', 
+            $valor, $desconto, '$novoNome', $categoriaId)";
+
+            // EXECUÇÃO DO SQL DE INSERÇÃO
+            $resultado = mysqli_query($conexao, $sql);
+
+            // REDIRECIONAR PARA INDEX
+            header('location: index.php');
+
             break;
 
         default:
